072a79ed5abed8995c7035354bd633fd166b29ab
trongnghiango <108252345+trongnghiango@users.noreply.github.com>
2026-01-23 10:45:38 +0700

    Reorder PATH export to append custom directories

--- nixos/modules/shell.nix
+++ nixos/modules/shell.nix
@@ -27,7 +27,7 @@
     # Thêm đường dẫn Scripts vào PATH
     # (Đảm bảo scripts trong dotfiles chạy được mà không cần symlink)
     #export PATH="$HOME/.dotfiles/bin/.local/bin:$HOME/.local/bin:$PATH"
-    export PATH="$HOME/.dotfiles/bin/.local/bin:$HOME/.dotfiles/user-bin/.local/bin:$HOME/.local/bin:$PATH"
+    export PATH="$PATH:$HOME/.dotfiles/bin/.local/bin:$HOME/.dotfiles/user-bin/.local/bin:$HOME/.local/bin"
 
     # Load profile cũ nếu có
     if [[ -f "$XDG_CONFIG_HOME/shell/profile" ]]; then
